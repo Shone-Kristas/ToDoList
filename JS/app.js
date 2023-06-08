43158d74ea0a33c22d06adb76b04e197a8c48d5f
Nick Bold <kristas21@yandex.ru>
2023-06-08 17:07:51 +0200

    second

--- JS/app.js
+++ JS/app.js
@@ -1,11 +1,10 @@
-VANTA.CELLS({
-    el: "#Habr",
-    mouseControls: true,
-    touchControls: true,
-    gyroControls: false,
-    minHeight: 200.00,
-    minWidth: 200.00,
-    scale: 1.00,
-    color1: 0x3400cc,
-    color2: 0xf23535
-  })
+VANTA.DOTS({
+  el: "#Habr",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00
+})
